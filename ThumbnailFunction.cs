a128640015321255ef9890704e649a18fadb6410
johnwalraven <johnwalraven28@gmail.com>
2022-04-09 18:03:07 +1200

    Thumbnail generation.
    Added code to generate a thumbail on an image being  added to a container.

--- ThumbnailFunction.cs
+++ ThumbnailFunction.cs
@@ -2,19 +2,75 @@
 // http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
 using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using Azure.Storage.Blobs;
+using ThumbnailFunctionApp;
 
 namespace johnwalraven.Thumbnail
 {
     public static class ThumbnailFunction
     {
+        private static readonly string BLOB_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+
+        /// <summary>
+        /// Creates a thumbnail and places it into another contatiner in blob storage when an image is added.
+        /// </summary>
+        /// <param name="eventGridEvent">The event sent from Azure Event Grid</param>
+        /// <param name="log">The logger</param>
+        /// <returns>void</returns>
         [FunctionName("ThumbnailFunction")]
-        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        public static async Task Run([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
         {
-            log.LogInformation(eventGridEvent.Data.ToString());
+            try
+            {
+                var createdEvent = ((JObject)eventGridEvent.Data).ToObject<StorageBlobCreatedEventData>();
+                var extension = Path.GetExtension(createdEvent.Url);
+                var encoder = ThumbnailHelper.GetEncoder(extension);
+
+                if (encoder != null)
+                {
+                    var thumbnailContainerName = Environment.GetEnvironmentVariable("THUMBNAIL_CONTAINER_NAME");
+                    var imagesContainerName = Environment.GetEnvironmentVariable("IMAGES_CONTAINER_NAME");
+
+                    var blobServiceClient = new BlobServiceClient(BLOB_STORAGE_CONNECTION_STRING);
+                    var blobContainerClient = blobServiceClient.GetBlobContainerClient(thumbnailContainerName);
+
+                    var blobName = ThumbnailHelper.GetBlobNameFromUrl(createdEvent.Url);
+
+                    var blob = new BlobClient(BLOB_STORAGE_CONNECTION_STRING, imagesContainerName, blobName);
+                    var stream = await blob.DownloadStreamingAsync();
+
+                    using var output = new MemoryStream();
+                    using Image image = await Image.LoadAsync(stream.Value.Content);
+
+                    var thumbnailWidth = Convert.ToInt32(Environment.GetEnvironmentVariable("THUMBNAIL_WIDTH"));
+                    var divisor = image.Width / thumbnailWidth;
+                    var height = Convert.ToInt32(Math.Round((decimal)(image.Height / divisor)));
+
+                    image.Mutate(x => x.Resize(thumbnailWidth, height));
+                    image.Save(output, encoder);
+                    output.Position = 0;
+
+                    await blobContainerClient.UploadBlobAsync(blobName, output);
+                }
+                else
+                {
+                    log.LogInformation($"No encoder support for: {createdEvent.Url}");
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogInformation(ex.Message);
+                throw;
+            }
+
         }
     }
 }
---
+++ ThumbnailHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Azure.Storage.Blobs;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Gif;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+
+namespace ThumbnailFunctionApp
+{
+    public class ThumbnailHelper
+    {
+        public static IImageEncoder GetEncoder(string extension)
+        {
+            IImageEncoder encoder = null;
+
+            extension = extension.Replace(".", "");
+
+            var isSupported = Regex.IsMatch(extension, "gif|png|jpe?g", RegexOptions.IgnoreCase);
+
+            if (isSupported)
+            {
+                switch (extension.ToLower())
+                {
+                    case "png":
+                        encoder = new PngEncoder();
+                        break;
+                    case "jpg":
+                    case "jpeg":
+                        encoder = new JpegEncoder();
+                        break;
+                    case "gif":
+                        encoder = new GifEncoder();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return encoder;
+        }
+
+        public static string GetBlobNameFromUrl(string blobUrl)
+        {
+            var uri = new Uri(blobUrl);
+            var blob = new BlobClient(uri);
+            return blob.Name;
+        }
+    }
+}
